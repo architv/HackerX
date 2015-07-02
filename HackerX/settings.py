3e93e10934855a72c31be267b17b162909085127
Archit Verma <architv07@gmail.com>
2015-07-02 08:56:31 +0000

    added some esthetic improvements

--- HackerX/settings.py
+++ HackerX/settings.py
@@ -116,3 +116,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIR = (
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'main/static'),
+)
+print STATICFILES_DIR
