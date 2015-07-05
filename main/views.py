d682bb0928d9a349559c4cd179ebc3a85887b5f9
Archit Verma <architv07@gmail.com>
2015-07-05 07:48:52 +0000

    edited .gitignore

--- main/views.py
+++ main/views.py
@@ -10,6 +10,7 @@ from operator import itemgetter
 # Create your views here.
 def home(request, user_name):
 	user = User.objects.get(user_name=user_name)
+	print user.required_github_data
 	if user.required_github_data:
 		return render(request, 'main/index.html', {'user_name': user_name, 'github_data': user.required_github_data})
 	
@@ -29,6 +30,7 @@ def home(request, user_name):
 		})
 	user.github_data = github_data
 	user.required_github_data = required_github_data
+	print required_github_data
 	user.save()
 	return render(request, 'main/index.html', {'user_name': user_name, 'github_data': required_github_data})
 
