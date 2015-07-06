fb9733df7dc1b8953601e993a98ad71fc0be0b16
Archit Verma <architv07@gmail.com>
2015-07-06 18:02:58 +0000

    created base template

--- HackerX/settings.py
+++ HackerX/settings.py
@@ -118,10 +118,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIR = (
+STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
     os.path.join(BASE_DIR, 'main/static'),
 )
-print STATICFILES_DIR
+print STATICFILES_DIRS
